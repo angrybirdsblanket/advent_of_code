83417b9447b714b09c83246f3d13a75860d383bd
= <=>
2024-12-26 02:34:18 +0800

    starting on day 8 debugging

--- pythonSol/2024/day8.py
+++ pythonSol/2024/day8.py
@@ -1,8 +1,24 @@
 from openFile import openFile
 
+def walls(tup_1, tup_2, data):
+    count = 0
+    direction = (-(tup_1[0] - tup_2[0]), -(tup_1[1] - tup_2[1]))
+    col = len(data)
+    length = len(data[0])
+    
+    if 0 <= tup_1[0] - direction[0] and 0 <= tup_1[1] - direction[1] < length and data[tup_1[0] - direction[0]][tup_1[1] - direction[1]] == ".":
+        count += 1
+        # print(tup_1, tup_2)
+
+    if col > tup_2[0] + direction[0] and 0 <= tup_2[1] + direction[1] < length and data[tup_2[0] + direction[0]][tup_2[1] + direction[1]] == ".":
+        count += 1
+        # print(tup_1, tup_2)
+
+    return count
+    
 data = []
 
-openFile(data, "data/day8test.txt")
+openFile(data, "data/day8.txt")
 
 indices = []
 for index, line in enumerate(data):
@@ -10,5 +26,10 @@ for index, line in enumerate(data):
         if line[i].isalnum():
             indices.append((index, i))
 
-for index in indices:
-    print(index)
+count = 0
+for i in range(len(indices)):
+    for j in range(i + 1, len(indices)):
+        if data[indices[i][0]][indices[i][1]] == data[indices[j][0]][indices[j][1]]:
+            count += walls(indices[i], indices[j], data)
+
+print(count)
