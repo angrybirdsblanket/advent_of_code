4255951bdfbe4dcc809df5bd25e6c812fb544c11
= <=>
2025-01-03 02:50:07 +0800

    finished days 10 and 11

--- pythonSol/2024/day10.py
+++ pythonSol/2024/day10.py
@@ -1,2 +1,42 @@
 from openFile import openFile
 
+data = []
+
+openFile(data, "data/day10.txt")
+
+def bfs(data, row, col):
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    queue = [(row, col, int(data[row][col]))]
+    row_length = len(data)
+    col_length = len(data[0])
+    visited = set()
+    count = 0
+
+    while len(queue) > 0:
+        curr_row, curr_col, num = queue.pop(0)
+        
+        if (curr_row, curr_col) in visited:
+            continue
+
+        visited.add((curr_row, curr_col)) 
+
+        if num == 9:
+            count += 1
+            continue
+        
+        for direction in directions:
+            new_row, new_col = curr_row + direction[0], curr_col + direction[1]
+            if 0 <= new_row < row_length and 0 <= new_col < col_length:
+                next_num = int(data[new_row][new_col])
+                if next_num == num + 1 and (new_row, new_col) not in visited:
+                    queue.append((new_row, new_col, next_num))
+    return count
+
+
+count = 0
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        if int(data[i][j]) == 0:
+           count += bfs(data, i, j)
+
+print(count)
---
+++ pythonSol/2024/day11.py
@@ -0,0 +1,29 @@
+from openFile import openFile
+
+data = []
+
+openFile(data, "data/day11.txt")
+
+def blink(line):
+    new_line = []
+    for num in line:
+        match num:
+            case 0:
+                new_line.append(1)
+            case _ if len(str(num)) % 2 == 0:
+                new_num, mid = str(num), len(str(num)) // 2
+                new_line.append(int(new_num[:mid]))
+                new_line.append(int(new_num[mid:]))
+            case _:
+                new_line.append(num * 2024)
+    return new_line
+
+data = data[0].split(" ")
+
+for i in range(len(data)):
+    data[i] = int(data[i])
+
+for i in range(25):
+    data = blink(data)
+
+print(len(data))
